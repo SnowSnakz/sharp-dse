0fd08bd0acc84fc9e20c45f4926ef254a1b852ba
Elemental Woof <snowsnakz@gmail.com>
2023-06-27 23:57:24 -0500

    Clean up test output

--- SharpDSE.Test/Program.cs
+++ SharpDSE.Test/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using SharpDSE;
+﻿using SharpDSE;
 using SharpDSE.SwdlChunks;
 
 Console.WriteLine("Print information about SWDL: (path to swdl file, or blank to skip this test)");
@@ -7,27 +6,63 @@ string? file = Console.ReadLine();
 
 if(file != null)
 {
+    // Remove surrounding quotation marks (quotes are added by dragging the file onto the command prompt window)
     if(file.StartsWith('"') && file.EndsWith('"'))
     {
         file = file[1..^1];
     }
 }
 
+// If no file is specified, skip this test.
 if (!string.IsNullOrWhiteSpace(file))
 {
+    // Otherwise, open the specified file
     using var fs = File.OpenRead(file);
 
+    // Try to load it into a managed Swdl object.
     Swdl swdl = new Swdl();
     swdl.Read(new BinaryReader(fs));
 
     Console.WriteLine();
 
-    Console.WriteLine($"File Name: {swdl.FileName}");
+    // Print some of the basic information contained within the file.
+    Console.WriteLine($"File Name:     {swdl.FileName}");
     Console.WriteLine($"Creation Date: {swdl.CreationDate}");
+    
+    // Print some information about the chunks contained within the file.
     Console.WriteLine($"Chunks:");
     foreach (SwdlChunk chunk in swdl)
     {
-        Console.WriteLine($"\t{chunk.LabelString.Replace("\x20", "\\x20")}: [Length = 0x{chunk.Length:x}]");
+        byte[] label = chunk.LabelBytes;
+
+        string type = "Unknown";
+        switch (chunk.LabelString)
+        {
+            case "pcmd":
+                type = "Pcm Data Chunk";
+                break;
+
+            case "wavi":
+                type = "Wave Information Chunk";
+                break;
+
+            case "prgi":
+                type = "Program Information Chunk";
+                break;
+
+            case "kgrp":
+                type = "Keygroup Chunk";
+                break;
+
+            case "eod\x20":
+                type = "End of Data";
+                break;
+        }
+
+        Console.WriteLine($"  At 0x{chunk.Offset:X8}: {type}");
+        Console.WriteLine($"    Label:  {chunk.LabelString} {{0x{label[0]:X2}, 0x{label[1]:X2}, 0x{label[2]:X2}, 0x{label[3]:X2}}}");
+        Console.WriteLine($"    Length: 0x{chunk.Length:X8}");
+
     }
 
 }
